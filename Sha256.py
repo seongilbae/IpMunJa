d8e9c9bb44f118229e1380606bd74dfe032d7aa5
Sib <beasongil@hanmail.net>
2020-03-27 01:09:11 +0900

    Add files via upload

---
+++ Sha256.py
@@ -0,0 +1,28 @@
+import hashlib
+import ttk
+from Tkinter import *
+
+tk = Tk()
+input_ = StringVar()
+
+def callback():
+    result = input_.get()
+    sha = hashlib.sha256(result.encode())
+    out = sha.hexdigest()
+    if len(result) == 0:
+        IN_la = Label(tk, text = "Input Plan Text again")
+        IN_la.pack()
+    else:
+        OUT_lb = Label(tk, text = out)
+        OUT_lb.pack()
+        
+tk.title("SHA 256 CONVERTER ")
+tk.geometry('500x600')
+IN_lb = Label(tk, text = "Input Plan Text")
+IN_lb.pack()
+textbox = ttk.Entry(tk, width=30, textvariable=input_)
+textbox.pack()
+button = Button(tk, text = "Translate", command = callback)
+button.pack()
+
+tk.mainloop()
