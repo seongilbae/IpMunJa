63b20c867977cc9e34fdd7f15205f835aa3a6109
Sib <beasongil@hanmail.net>
2020-04-03 07:51:31 +0900

    Add files via upload

---
+++ chapter12/Fuzzer.py
@@ -0,0 +1,35 @@
+import sys
+import subprocess
+
+diction = "abcdefghijklmnopqrstuvwxyz"
+count = 0
+
+        
+def run_(cmd):
+    global count
+    result = subprocess.call(cmd, stdout=subprocess.PIPE)
+    if(result == 1 ):
+        count = count + 1
+        print("########TestCase%d#########" % count)
+        print("Input :  %s" % cmd[16:])
+        print("Result : FAILED!\n")
+        return 1
+    else:
+        count = count + 1
+        print("########TestCase%d#########" % count)
+        print("Input :  %s" % cmd[16:])
+        print("Result : SUCCESS!\n")
+        return 0
+        
+
+        
+def main():
+    
+    for i in list(diction):
+        for j in list(diction):
+            k=i+j
+            ans= ''.join(k)
+            if(run_("D:\\LetsFuzz.exe %s"% ans)==0): #LetsFuzz file location
+                return 0
+
+main()
